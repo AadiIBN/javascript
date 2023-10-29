fef4e50318562764567820968fd0f1bfc592fc31
AadiIBN <114740302+AadiIBN@users.noreply.github.com>
2023-10-29 07:23:07 +0000

    variable

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountID=144553
+let accountEmail="anujibn@gmail.com"
+var accountPassword="12345"
+accountCity="Muzaffarnagar"
+// accountID=112233 not allowed
+console.log(accountID);
+console.table([accountID,accountEmail,accountPassword,accountCity])
+
